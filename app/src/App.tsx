19ad460ab7c9c8ba572a74134933ffbb09ae50fb
nicholascannon <nicholascannon1@gmail.com>
2024-01-09 19:52:50 +1100

    reset styles

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,19 +1,11 @@
-import { ThemeProvider } from '@emotion/react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-
-import { GlobalStyles } from './styles/global';
-import { THEME } from './styles/theme';
 
 export const App = () => {
     return (
-        <ThemeProvider theme={THEME}>
-            <GlobalStyles />
-
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/*" element={<h1>Hello, world</h1>} />
-                </Routes>
-            </BrowserRouter>
-        </ThemeProvider>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/*" element={<h1>Hello, world</h1>} />
+            </Routes>
+        </BrowserRouter>
     );
 };
