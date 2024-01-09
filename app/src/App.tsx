3aabb631c59367385123868ff40b18598d5377e0
nicholascannon <nicholascannon1@gmail.com>
2024-01-09 20:00:32 +1100

    Setup chakra UI

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,11 +1,14 @@
+import { ChakraProvider } from '@chakra-ui/react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 export const App = () => {
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/*" element={<h1>Hello, world</h1>} />
-            </Routes>
-        </BrowserRouter>
+        <ChakraProvider>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/*" element={<h1>Hello, world</h1>} />
+                </Routes>
+            </BrowserRouter>
+        </ChakraProvider>
     );
 };
