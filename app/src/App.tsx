5d3e7d07725437c86d8e92fc3c0f098d06ab0343
nicholascannon <nicholascannon1@gmail.com>
2024-01-27 21:09:22 +1100

    Router

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,15 +1,10 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-
+import { Router } from '@/router';
 import { ThemeProvider } from '@/theme';
 
 export const App = () => {
     return (
         <ThemeProvider>
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/*" element={<h1>Hello, world</h1>} />
-                </Routes>
-            </BrowserRouter>
+            <Router />
         </ThemeProvider>
     );
 };
