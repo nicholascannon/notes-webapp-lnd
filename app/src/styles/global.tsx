0e949bc868aba5ac612cb0e8ba1f5f4377ed6471
nicholascannon <nicholascannon1@gmail.com>
2024-01-27 13:55:36 +1100

    update global reset

--- app/src/styles/global.tsx
+++ app/src/styles/global.tsx
@@ -1,35 +1,82 @@
-import { Global } from '@emotion/react';
+import { Global, css } from '@emotion/react';
 
 export const GlobalStyles = () => (
     <Global
-        styles={(theme) => ({
-            '*, *::before, *::after': {
-                boxSizing: 'border-box',
-            },
-            '*': {
-                margin: 0,
-            },
-            body: {
-                width: '100vw',
-                height: '100vh',
-                lineHeight: 1.5,
-                WebkitFontSmoothing: 'antialiased',
-                color: theme.colors.foreground,
-                backgroundColor: theme.colors.background,
-            },
-            '#root': {
-                height: '100%',
-            },
-            'img, picture, video, canvas, svg': {
-                display: 'block',
-                maxWidth: '100%',
-            },
-            'input, button, textarea, select': {
-                font: 'inherit',
-            },
-            'p, h1, h2, h3, h4, h5, h6': {
-                overflowWrap: 'break-word',
-            },
-        })}
+        styles={() => css`
+            /* Box sizing rules */
+            *,
+            *::before,
+            *::after {
+                box-sizing: border-box;
+            }
+
+            /* Remove default margin */
+            body,
+            h1,
+            h2,
+            h3,
+            h4,
+            p,
+            figure,
+            blockquote,
+            dl,
+            dd {
+                margin-block-end: 0;
+            }
+
+            /* Remove list styles on ul, ol elements with a list role, which suggests default styling will be removed */
+            ul[role='list'],
+            ol[role='list'] {
+                list-style: none;
+            }
+
+            /* Set core root defaults */
+            html:focus-within {
+                scroll-behavior: smooth;
+            }
+
+            /* Set core body defaults */
+            body {
+                min-height: 100vh;
+                text-rendering: optimizeSpeed;
+                line-height: 1.5;
+            }
+
+            /* A elements that don't have a class get default styles */
+            a:not([class]) {
+                text-decoration-skip-ink: auto;
+            }
+
+            /* Make images easier to work with */
+            img,
+            picture {
+                max-width: 100%;
+                display: block;
+            }
+
+            /* Inherit fonts for inputs and buttons */
+            input,
+            button,
+            textarea,
+            select {
+                font: inherit;
+            }
+
+            /* Remove all animations, transitions and smooth scroll for people that prefer not to see them */
+            @media (prefers-reduced-motion: reduce) {
+                html:focus-within {
+                    scroll-behavior: auto;
+                }
+
+                *,
+                *::before,
+                *::after {
+                    animation-duration: 0.01ms !important;
+                    animation-iteration-count: 1 !important;
+                    transition-duration: 0.01ms !important;
+                    scroll-behavior: auto !important;
+                }
+            }
+        `}
     />
 );
