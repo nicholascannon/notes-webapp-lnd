86a8d93aa2820fac12108d01ae7233b6cc73a734
nicholascannon <nicholascannon1@gmail.com>
2024-01-27 13:40:37 +1100

    Revert "Setup chakra UI"
    This reverts commit 3aabb631c59367385123868ff40b18598d5377e0.

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,14 +1,11 @@
-import { ChakraProvider } from '@chakra-ui/react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 export const App = () => {
     return (
-        <ChakraProvider>
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/*" element={<h1>Hello, world</h1>} />
-                </Routes>
-            </BrowserRouter>
-        </ChakraProvider>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/*" element={<h1>Hello, world</h1>} />
+            </Routes>
+        </BrowserRouter>
     );
 };
