00af52b96702905e147db1eb3e9a023d13da7150
nicholascannon <nicholascannon1@gmail.com>
2024-04-13 14:58:48 +1000

    Setup notes feature

--- app/src/router/Router.tsx
+++ app/src/router/Router.tsx
@@ -1,23 +1,11 @@
-import { motion } from 'framer-motion';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { NotesPage } from '@/features/notes';
 
 export const Router = () => {
     return (
         <BrowserRouter>
             <Routes>
-                <Route
-                    path="/"
-                    element={
-                        <motion.h1
-                            initial={{ opacity: 0.01, y: 50 }}
-                            animate={{ opacity: 1, y: 0 }}
-                            transition={{ duration: 0.25, ease: 'easeOut' }}
-                        >
-                            Hello, world
-                        </motion.h1>
-                    }
-                />
-                <Route path="*" element={<Navigate to="/" replace />} />
+                <Route path="/*" element={<NotesPage />} />
             </Routes>
         </BrowserRouter>
     );
