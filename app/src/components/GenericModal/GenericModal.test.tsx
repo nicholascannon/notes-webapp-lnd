d9482b46e0784b98bd241176b98154f64b44be92
nicholascannon <nicholascannon1@gmail.com>
2024-04-13 15:16:54 +1000

    GenericModal tests

--- app/src/components/GenericModal/GenericModal.test.tsx
+++ app/src/components/GenericModal/GenericModal.test.tsx
@@ -1,1 +1,34 @@
-describe('<GenericModal />', () => {});
+import { userEvent } from '@testing-library/user-event';
+import { GenericModal } from '.';
+import { render, screen } from '@/utils/testing';
+
+describe('<GenericModal />', () => {
+    it('should render modal and overlay', () => {
+        render(
+            <GenericModal>
+                <p>Content</p>
+            </GenericModal>,
+        );
+
+        expect(screen.getByTestId('generic-modal-overlay')).toBeVisible();
+        expect(screen.getByTestId('generic-modal')).toBeVisible();
+        expect(screen.getByText('Content')).toBeVisible();
+    });
+
+    it('should call onModalClose when clicking outside the modal', async () => {
+        const onModalClose = jest.fn();
+        render(
+            <GenericModal onModalClose={onModalClose}>
+                <p>Content</p>
+            </GenericModal>,
+        );
+
+        await userEvent.click(screen.getByTestId('generic-modal'));
+
+        expect(onModalClose).not.toHaveBeenCalled();
+
+        await userEvent.click(screen.getByTestId('generic-modal-overlay'));
+
+        expect(onModalClose).toHaveBeenCalled();
+    });
+});
