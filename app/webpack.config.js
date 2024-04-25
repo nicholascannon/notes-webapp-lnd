67a18659a15548790256e6e6b95db656bd27b87b
nicholascannon <nicholascannon1@gmail.com>
2024-04-25 10:33:02 +1000

    use file-loader for fontsource fonts

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -38,6 +38,10 @@ module.exports = {
                 use: 'babel-loader',
             },
             {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                use: 'file-loader',
+            },
+            {
                 test: /\.css$/i,
                 use: ['style-loader', 'css-loader'],
             },
