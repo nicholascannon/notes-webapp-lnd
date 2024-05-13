44ac49bb61c4199b5a45f3efb6900564b367046f
nicholascannon <nicholascannon1@gmail.com>
2024-05-13 19:23:19 +1000

    Add footer with github link

---
+++ app/src/components/Footer/Footer.tsx
@@ -0,0 +1,30 @@
+import styled from '@emotion/styled';
+
+export const Footer = () => {
+    return (
+        <StyledFooter>
+            <p>
+                View the code{' '}
+                <a href="https://github.com/nicholascannon/notes-webapp-lnd">
+                    here
+                </a>
+                .
+            </p>
+        </StyledFooter>
+    );
+};
+
+const StyledFooter = styled.footer(({ theme }) => ({
+    position: 'fixed',
+    right: 0,
+    left: 0,
+    bottom: 0,
+    width: '100vw',
+    display: 'flex',
+    justifyContent: 'center',
+
+    padding: theme.sizes[4],
+    '& *': {
+        color: theme.colors.greys[0],
+    },
+}));
--- app/src/components/Layout/Layout.tsx
+++ app/src/components/Layout/Layout.tsx
@@ -1,15 +1,16 @@
 import { ReactNode } from 'react';
+import { Footer } from '../Footer';
 import { theme } from '@/theme';
 
 export const Layout = ({ children }: { children: ReactNode }) => {
     return (
         <main
             css={{
-                minHeight: '100vh',
                 padding: theme.sizes[16],
             }}
         >
             {children}
+            <Footer />
         </main>
     );
 };
