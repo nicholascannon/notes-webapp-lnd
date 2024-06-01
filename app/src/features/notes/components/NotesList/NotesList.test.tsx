2a84a8384b14029b0df1f0d8e3065f80ca244141
nicholascannon <nicholascannon1@gmail.com>
2024-06-01 13:01:34 +1000

    Add test for empty notes page

--- app/src/features/notes/components/NotesList/NotesList.test.tsx
+++ app/src/features/notes/components/NotesList/NotesList.test.tsx
@@ -6,24 +6,36 @@ import { render, screen } from '@/utils/testing';
 describe('<NotesList />', () => {
     const navigate = mockNavigate();
 
-    beforeEach(() => {
-        render(
-            <NotesList
-                notes={[
-                    { id: '1', lastUpdate: new Date(), text: 'Note 1' },
-                    { id: '2', lastUpdate: new Date(), text: 'Note 2' },
-                    { id: '3', lastUpdate: new Date(), text: 'Note 3' },
-                ]}
-            />,
-        );
+    describe('with notes', () => {
+        beforeEach(() => {
+            render(
+                <NotesList
+                    notes={[
+                        { id: '1', lastUpdate: new Date(), text: 'Note 1' },
+                        { id: '2', lastUpdate: new Date(), text: 'Note 2' },
+                        { id: '3', lastUpdate: new Date(), text: 'Note 3' },
+                    ]}
+                />,
+            );
+        });
+
+        it('should render all compact notes', () => {
+            expect(screen.getAllByTestId('compact-note').length).toBe(3);
+        });
+
+        it('should navigate to note details on click', async () => {
+            await userEvent.click(screen.getByText('Note 1'));
+            expect(navigate).toHaveBeenCalledWith('/note/1');
+        });
     });
 
-    it('should render all compact notes', () => {
-        expect(screen.getAllByTestId('compact-note').length).toBe(3);
-    });
+    describe('without notes', () => {
+        beforeEach(() => {
+            render(<NotesList notes={[]} />);
+        });
 
-    it('should navigate to note details on click', async () => {
-        await userEvent.click(screen.getByText('Note 1'));
-        expect(navigate).toHaveBeenCalledWith('/note/1');
+        it('should render help text', () => {
+            expect(screen.getByText('Press + to add a note')).toBeVisible();
+        });
     });
 });
