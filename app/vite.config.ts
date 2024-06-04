2277029dbe491346c3c872376db940d93f8cc267
nicholascannon <nicholascannon1@gmail.com>
2024-06-04 17:39:33 +1000

    Setup vite

---
+++ app/vite.config.ts
@@ -0,0 +1,23 @@
+import path from 'path';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    server: {
+        port: 3000,
+    },
+    plugins: [
+        react({
+            babel: {
+                plugins: ['@emotion/babel-plugin'],
+            },
+            jsxImportSource: '@emotion/react',
+        }),
+    ],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, 'src'),
+        },
+    },
+});
